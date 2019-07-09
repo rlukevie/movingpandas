c66c99b2d52146aeb509c10e1f60adff09d3beda
Anita Graser <anitagraser@gmx.at>
2019-07-09 21:04:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,12 +22,10 @@ setup(name='movingpandas',
       classifiers=[
             'License :: OSI Approved :: BSD License',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3 :: Only',
             'Programming Language :: Python :: Implementation :: CPython',
       ],
-      python_requires='>=3.5',
+      python_requires='==3.6',
       install_requires=install_requires,
       zip_safe=False)
